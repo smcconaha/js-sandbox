f0a9bdf6e9fedf838ab1d21ce7e10f1079d58efb
smcconaha <steve.mcconaha@student.awesomeinc.org>
2022-09-13 18:35:00 +0000

    Adding even more sand

--- displayMessage/script.js
+++ displayMessage/script.js
@@ -1,11 +1,30 @@
-const button =document.getElementById('buttonSub');//ID, notice no #
-const textIn =document.querySelector('#textIn');//ID
-const output =document.querySelector('.output');//class
+const btn = document.querySelector('button');
+btn.addEventListener ('click', () => displayMessage('Your inbox is almost full — delete some mails', 'warning'));
 
-function greeting (name) {
-  name = textIn.textContent;
-  let message = `Hello ${name} how are you today?`;
-  output.textContent = message;
+function displayMessage(msgText, msgType) {
+  const body = document.body;
+  const panel = document.createElement ('div');
+  panel.setAttribute('class','msgBox');
+  body.appendChild(panel);
+
+  const msg = document.createElement('p');
+  msg.textContent = msgText;
+  panel.appendChild(msg);
+
+  const closeBtn = document.createElement('button');
+  closeBtn.textContent = 'x';
+  panel.appendChild(closeBtn);
+
+  closeBtn.addEventListener ('click', () =>
+  panel.parentNode.removeChild(panel));
+
+  if (msgType === 'warning') {
+    msg.style.backgroundImage = 'url(icons/warning.png)';
+    panel.style.backgroundColor = 'red';
+  } else if (msgType === 'chat') {
+    msg.style.backgroundImage = 'url(icons/chat.png)';
+    panel.style.backgroundColor = 'aqua';
+  } else {
+    msg.style.paddingLeft = '20px';
+  }
 };
-button.addEventListener('click',greeting);
-console.log(textIn.textContent);
