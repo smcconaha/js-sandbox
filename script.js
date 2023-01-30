e84bc9842ac0b20c45ba98545bfbcad78b2f5bbf
smcconaha <steve.mcconaha@student.awesomeinc.org>
2023-01-30 19:42:27 +0000

    Added API exercise, currently pulling characters and various data from rickandmorty API

--- script.js
+++ script.js
@@ -1,61 +1,66 @@
-const person = {
-    name: {
-        first: "Bob",
-        last: "Potato",
-    },
-    age: 31,
-    bio() {
-      console.log(`${this.name.first} ${this.name.last} is ${this.age} years old.`);
-    },
-    introduceSelf() {
-      console.log(`Hi! I'm ${this.name.first}.`);
-    },
-  };
+async function populate() {
+  //will use the Fetch API to retrieve JSON from the server
+  const requestURL = 'https://rickandmortyapi.com/api/character';
+  //initiating a new request object
+  const request = new Request(requestURL);
 
+  //fetch method makes network request and returns Response object
+  const response = await fetch(request);
+  //response retrieved as JSON using json() fn of the Response object
+  const characters = await response.json();
 
-//adding new member name and value to person object  
-const myDataName = "height";
-const myDataValue = "1.75m";
-person[myDataName] = myDataValue;
+  populateHeader(characters);
+  populateCharacters(characters);
+  // console.log(characters)
 
-const weight = "weight";
-const weightValue = "132lb";
-person[weight] = weightValue;
-
-  person.age = 51;
-  person["name"]["last"] = "Beef";
-
-function createCar(make, model, year) {
-  const obj = {};
-  obj.make = make;
-  obj.model = model;
-  obj.year = year;
-  obj.introduceStmnt = function () {
-    console.log(`I am the owner of a ${this.year} ${this.make}, ${this.model}`);
-  };
-  return obj;
 }
 
-//example of creating emty object, initializing it, and returning it
-const myCar = createCar("Honda", "Civic", 2011);
-myCar.make;
-myCar.model;
-myCar.year;
-myCar.introduceStmnt;
-
-//Constructor, a function called using the new keyword
-
-function Person(name) { //note the capitalization
-  this.name = name;
-  this.introduceSelf = function () {
-    console.log(`Hi, my name is ${this.name}.`);
-  };
+function populateHeader(obj) {
+  const header = document.querySelector('header');
+  const myH1 = document.createElement('h1');
+  myH1.textContent = 'Rick and Morty Characters';
+  header.appendChild(myH1);
 }
 
-const frank = new Person("Frank");
-frank.name;
-frank.introduceSelf();
+function populateCharacters(obj) {
+  const section = document.querySelector('section');
+  const characters = obj.results;
+  // console.log(characters)
 
-const jill = new Person("Jill");
-jill.name;
-jill.introduceSelf;
+  for (const char of characters) {
+    const myArticle = document.createElement('article');
+    const myH2 = document.createElement('h2');
+    const myPara1 = document.createElement('p');
+    const myPara2 = document.createElement('p');
+    const myPara3 = document.createElement('p');
+    const myPara4 = document.createElement('p');
+    const myPara5 = document.createElement('p');
+    const myList = document.createElement('ul');
+
+    myH2.textContent = char.name;
+    myPara1.textContent = `Name: ${char.name}`;
+    myPara2.textContent = `Status: ${char.status}`;
+    myPara3.textContent = `Species: ${char.species}`;
+    myPara4.textContent = `Location: ${char.location.name}`;
+    // myPara5.textContent = 'Episodes:';
+
+    // const characterEpisodes = char.episode;
+    // for (const item of episodes) {
+    //   const listItem = document.createElement('li');
+    //   listItem.textContent = item;
+    //   myList.appendChild(listItem);
+    // }
+
+    myArticle.appendChild(myH2);
+    myArticle.appendChild(myPara1);
+    myArticle.appendChild(myPara2);
+    myArticle.appendChild(myPara3);
+    myArticle.appendChild(myPara4);    
+    myArticle.appendChild(myPara5);
+    myArticle.appendChild(myList);
+
+    section.appendChild(myArticle);
+  }
+}
+
+populate();
